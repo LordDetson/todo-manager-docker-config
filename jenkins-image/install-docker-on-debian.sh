d20a9ded86ce5da6a7c8298d841bd95eadb09692
LordDetson <46191868+LordDetson@users.noreply.github.com>
2023-01-29 22:00:48 +0400

    Update install-docker-on-debian.sh

--- jenkins-image/install-docker-on-debian.sh
+++ jenkins-image/install-docker-on-debian.sh
@@ -15,6 +15,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo "Set up the repository"
+echo "arch=$(dpkg --print-architecture)"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) nightly" | tee /etc/apt/sources.list.d/docker.list
