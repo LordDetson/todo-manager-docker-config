4c960aa7fb9f5d5b14bbecc0da8e3cea9f4442f3
LordDetson <46191868+LordDetson@users.noreply.github.com>
2023-01-29 22:19:01 +0400

    Added the second apt package index

--- jenkins-image/install-docker-on-debian.sh
+++ jenkins-image/install-docker-on-debian.sh
@@ -15,11 +15,12 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo "Set up the repository"
-echo "arch=$(dpkg --print-architecture)"
-echo "lsb_release=$(lsb_release -cs)"
 echo \
-  "deb [arch=x86_64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+echo "Update the apt package index"
+apt-get -qq update
   
 echo "Install Docker Engine, containerd, and Docker Compose"
 apt-get install -y \
