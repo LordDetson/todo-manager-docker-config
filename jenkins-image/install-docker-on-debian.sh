4c9d7897b605d5032e17b1d106b3a96bf57a9b95
LordDetson <46191868+LordDetson@users.noreply.github.com>
2023-01-29 22:10:49 +0400

    Update install-docker-on-debian.sh

--- jenkins-image/install-docker-on-debian.sh
+++ jenkins-image/install-docker-on-debian.sh
@@ -16,9 +16,10 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 
 echo "Set up the repository"
 echo "arch=$(dpkg --print-architecture)"
+echo "lsb_release=$(lsb_release -cs)"
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) nightly" | tee /etc/apt/sources.list.d/docker.list
+  "deb [arch=x86_64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 echo "Install Docker Engine, containerd, and Docker Compose"
 apt-get install -y \
